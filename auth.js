87cee1a57ae70da4e0eb53e6618d61ac8658c6c8
alessandrokonopacki01 <alessandrokonopacki@gmail.com>
2026-06-02 15:49:05 -0300

    s

--- auth.js
+++ auth.js
@@ -3,14 +3,14 @@ import {
     GoogleAuthProvider,
     signInWithPopup
 }
-from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+    from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 import {
     doc,
     getDoc,
     setDoc,
     Timestamp
 }
-from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+    from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 import { db } from "./firebase.js";
 const provider = new GoogleAuthProvider();
 const btnLogin =
@@ -24,19 +24,19 @@ btnLogin.addEventListener("click", async () => {
             );
         const usuario = resultado.user;
         const usuarioRef =
-    doc(db, "usuarios", usuario.uid);
+            doc(db, "usuarios", usuario.uid);
 
-const usuarioDoc =
-    await getDoc(usuarioRef);
-if (!usuarioDoc.exists()) {
-    await setDoc(usuarioRef, {
-        nome: usuario.displayName,
-        email: usuario.email,
-        creditos: 5,
-        tipo: "profissional",
-        criadoEm: Timestamp.now()
-    });
-}
+        const usuarioDoc =
+            await getDoc(usuarioRef);
+        if (!usuarioDoc.exists()) {
+            await setDoc(usuarioRef, {
+                nome: usuario.displayName,
+                email: usuario.email,
+                creditos: 5,
+                tipo: "profissional",
+                criadoEm: Timestamp.now()
+            });
+        }
         alert(
             "Bem-vindo, " +
             usuario.displayName
