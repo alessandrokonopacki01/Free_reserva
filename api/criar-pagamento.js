9ad5710e7458cecd10a53f9a6e2209331402710c
alessandrokonopacki01 <alessandrokonopacki@gmail.com>
2026-06-03 21:55:39 -0300

    sssssssss

--- api/criar-pagamento.js
+++ api/criar-pagamento.js
@@ -3,50 +3,8 @@ export default async function handler(req, res) {
     const token =
         process.env.MERCADOPAGO_ACCESS_TOKEN;
 
-    try {
-
-        const resposta =
-            await fetch(
-                "https://api.mercadopago.com/v1/payments",
-                {
-                    method: "POST",
-                    headers: {
-                        "Authorization":
-                            `Bearer ${token}`,
-                        "Content-Type":
-                            "application/json"
-                    },
-                    body: JSON.stringify({
-
-                        transaction_amount: 4.90,
-
-                        description:
-                            "5 Créditos Contrata Reserva",
-
-                        payment_method_id:
-                            "pix",
-
-                        payer: {
-                            email:
-                                "teste@contratareserva.com"
-                        }
-
-                    })
-                }
-            );
-
-        const dados =
-            await resposta.json();
-
-        res.status(200).json(dados);
-
-    }
-    catch (erro) {
-
-        res.status(500).json({
-            erro: erro.message
-        });
-
-    }
+    res.status(200).json({
+        inicioToken: token.substring(0, 15)
+    });
 
 }
