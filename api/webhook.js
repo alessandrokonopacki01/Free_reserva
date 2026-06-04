ddf6b1581b2af8055e09fb6181c738f8860ba0ee
alessandrokonopacki01 <alessandrokonopacki@gmail.com>
2026-06-04 20:43:17 +0000

    abcd

--- api/webhook.js
+++ api/webhook.js
@@ -4,17 +4,18 @@ export default async function handler(req, res) {
 
     try {
 
-        console.log("WEBHOOK RECEBIDO");
-
-        console.log(req.body);
+        await db
+            .collection("logs_webhook")
+            .add({
+                recebidoEm: new Date(),
+                dados: req.body
+            });
 
         return res.status(200).json({
             recebido: true
         });
 
     } catch (erro) {
-
-        console.error(erro);
 
         return res.status(500).json({
             erro: erro.message
