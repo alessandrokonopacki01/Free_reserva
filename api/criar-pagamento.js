258ed409329027a5ced58e7a53b65d4eea6dfe54
alessandrokonopacki01 <alessandrokonopacki@gmail.com>
2026-06-03 22:00:12 -0300

    s

--- api/criar-pagamento.js
+++ api/criar-pagamento.js
@@ -1,10 +1,3 @@
-export default async function handler(req, res) {
-
-    const token =
-        process.env.MERCADOPAGO_ACCESS_TOKEN;
-
-    res.status(200).json({
-        inicioToken: token.substring(0, 15)
-    });
-
-}
+res.status(200).json({
+    inicioToken: token.substring(0, 15)
+});
